fc5e0e958c932e993fb39b4629548f5b9b611bdd
Linus Shaddai <awajisomegak@gmail.com>
2023-08-15 16:07:23 +0100

    Use session storage to store and retrieve token

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from './components/Preferences/Preferences';
 import Login from './components/Login/Login';
 
-function App() {
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
 
-  const [token, setToken] = useState();
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
+function App() {
+  // const [token, setToken] = useState();
+  const token = getToken();
 
   if(!token) {
     return <Login setToken={setToken} />
